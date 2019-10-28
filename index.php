9a8adc8a15116dfb21e1889f4fb1640e6692e9d3
DESKTOP-EJSDS5U\kazuy <ochi0612@gmail.com>
2019-10-28 21:02:46 +0900

    wp add

--- index.php
+++ index.php
@@ -1,6 +1,54 @@
-test
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="./style.css">
+</head>
+<body>
 
-<?php
+<header>
+    <div class="header">
+        <a class="header_logo" href="<?php ?>">
+            <img src="./imgs/logo.png" alt="ochi" />
+        </a>
+        <ul class="header_nav">
+            <li>
+                <a href="">aaa</a>
+            </li>
+            <li>
+                <a href="">bbb</a>
+            </li>
+            <li>
+                <a href="">ccc</a>
+            </li>
+            <li>
+                <a href="">ddd</a>
+            </li>
+            <li>
+                <a href="">eee</a>
+            </li>
+        </ul>
+    </div>
+</header>
+<footer>
+    <div class="footer">
+        <a  class="footer_logo" href="<?php ?>">
+            <img src="https://images.cooltext.com/5308363.png" alt="ochi" />
+        </a>
+        <ul class="footer_nav">
+            <li><a hlef="">aaa</a></li>
+            <li><a hlef="">bbb</a></li>
+            <li><a hlef="">ccc</a></li>
+            <li><a hlef="">ddd</a></li>
+            <li><a hlef="">eee</a></li>
+        </ul>
+        <p class="footer_copywriter">@Copywriter</p>
+    </div>
+    <script src="https://cdn.rawgit.com/google/code-prettify/master/loader/run_prettify.js"></script>
+</footer>
+</body>
 
-echo "Hello, Heroku!!!";
-
+</html>
