0c624d9f366a608f1255c05e7aec3ac7522f8e35
DESKTOP-EJSDS5U\kazuy <ochi0612@gmail.com>
2019-10-28 22:53:00 +0900

    add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -6,11 +6,11 @@ var themeDir = '';
 
 // 以下を追記
 gulp.task('sass', function () {
-    return gulp.src(themeDir + '/sass/style.scss')
+    return gulp.src('sass/style.scss') // コンパイル後の読込先
         .pipe(sass())
-        .pipe(gulp.dest(themeDir));
+        .pipe(gulp.dest('./')); // コンパイル結果のディレクトリ
 });
 
 gulp.task('default', ['sass'], function () {
-    gulp.watch(themeDir + '/sass/*.scss', ['sass']);
+    gulp.watch('sass/*.scss', ['sass']); // ファイルが更新されるたび動かす
 });
