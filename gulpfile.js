7686549fdbebc4c12a2a80e1be73d87a98933517
DESKTOP-EJSDS5U\kazuy <ochi0612@gmail.com>
2019-10-31 22:39:00 +0900

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,18 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');  //プラグインの定義
 var themeDir = '';
 
 // 以下を追記
 gulp.task('sass', function () {
     return gulp.src('lib/sass/style.scss') // コンパイル後の読込先
-        .pipe(sass())
-        .pipe(gulp.dest('./')); // コンパイル結果のディレクトリ
+    .pipe(sass())
+    .pipe(autoprefixer({
+        browsers: ["last 2 versions"],
+        cascade: false
+    }))  //autoprefixerの実行
+    .pipe(gulp.dest('./')) // コンパイル結果のディレクトリ
 });
 
 gulp.task('default', ['sass'], function () {
