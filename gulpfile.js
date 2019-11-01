ade83ba60fbf8ccdd05ac9c9143abda9ab6ceb09
DESKTOP-EJSDS5U\kazuy <ochi0612@gmail.com>
2019-11-01 19:13:42 +0900

    bgjfdlnjkkdlnsfjnjk

--- gulpfile.js
+++ gulpfile.js
@@ -2,17 +2,17 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-var autoprefixer = require('gulp-autoprefixer');  //プラグインの定義
+// var autoprefixer = require('gulp-autoprefixer');  //プラグインの定義
 var themeDir = '';
 
 // 以下を追記
 gulp.task('sass', function () {
     return gulp.src('lib/sass/style.scss') // コンパイル後の読込先
     .pipe(sass())
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions', 'iOS >= 8.1'],
-        cascade: false
-    }))  //autoprefixerの実行
+    // .pipe(autoprefixer({
+    //     browsers: ['last 2 versions', 'iOS >= 8.1'],
+    //     cascade: false
+    // }))  //autoprefixerの実行
     .pipe(gulp.dest('./')) // コンパイル結果のディレクトリ
 });
 
