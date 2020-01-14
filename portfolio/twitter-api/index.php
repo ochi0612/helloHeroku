78f019ba82cc27e88c43e5843af53c5b94f407e8
mediaplex-ochi <k-ochi@mediaplex.co.jp>
2020-01-14 11:53:22 +0900

    CRC追加

--- portfolio/twitter-api/index.php
+++ portfolio/twitter-api/index.php
@@ -1,12 +1,12 @@
 <?php
 
-// // CRC試験用
-// if(isset($_REQUEST['crc_token'])) {
-//   $signature = hash_hmac('sha256', $_REQUEST['crc_token'], 'lyP5QcjEPEoopezvrAovGqo6HgHXFcsqlhF63Q9xxM1nsjiJHJ', true);
-//   $response['response_token'] = 'sha256='.base64_encode($signature);
-//   print json_encode($response);
-//   exit;
-// }
+// CRC試験用
+if(isset($_REQUEST['crc_token'])) {
+  $signature = hash_hmac('sha256', $_REQUEST['crc_token'], 'lyP5QcjEPEoopezvrAovGqo6HgHXFcsqlhF63Q9xxM1nsjiJHJ', true);
+  $response['response_token'] = 'sha256='.base64_encode($signature);
+  print json_encode($response);
+  exit;
+}
 
 // {
 //   "id": "1215560020235997184",
