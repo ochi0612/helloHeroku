30bc5065cff326be333a8f90e0684e797a49080d
mediaplex-ochi <k-ochi@mediaplex.co.jp>
2020-01-29 11:16:44 +0900

    閉店検索

--- portfolio/twitter-api/close-store-map.php
+++ portfolio/twitter-api/close-store-map.php
@@ -1,3 +1,27 @@
+<?php
+require_once('index.php');
+
+$connect = Twitter_OAuth();
+
 // ハッシュタグによるツイート検索
-$hash_params = ['q' => '#html5,#css3' ,'count' => '10', 'lang'=>'ja'];
-$hash = $connection->get('search/tweets', $hash_params)->statuses;
+$search_hash = $connect->get(
+    'search/tweets',
+    array(
+        'q' => '#閉店セール　OR　#閉店' ,
+        'count' => '100',
+        'lang'=>'ja'
+    )
+)->statuses;
+
+
+foreach ($search_hash as $value) {
+    if ($value->user->followers_count > 500) {
+        echo '<pre>';
+        print_r($value->created_at." ");
+        print_r($value->user->screen_name." ");
+        print_r($value);
+        echo '</pre>';
+    }
+    // $value->user->followers_count
+    // exit;
+}
